8fc2530036378149c4a8976e7c27e0931b4ec996
Courtney Fay <cfay@nexthealthtechnologies.com>
2020-09-17 16:50:25 -0600

    Making widgets resizable

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -19,9 +19,9 @@ const Dashboard = () => {
   };
 
   const layout = [
-    {x: 0, y: 0, w: 6, h: 13},
-    {x: 6, y: 0, w: 6, h: 13},
-    {x: 3, y: 15, w: 6, h: 30},
+    {x: 0, y: 0, w: 6, h: 13, minW: 6, minH: 13, isResizable: true, isDraggable: true},
+    {x: 6, y: 0, w: 6, h: 13, minW: 6, minH: 13, isResizable: true, isDraggable: true},
+    {x: 3, y: 15, w: 6, h: 30, minW: 6, minH: 30, isResizable: true, isDraggable: true},
   ]
 
   return (
@@ -32,6 +32,7 @@ const Dashboard = () => {
         cols={12}
         rowHeight={30}
         width={1200}
+        verticalCompact={true}
         onLayoutChange={handleLayoutChange}
       >
         <WidgetStyle key="a" data-grid={layout[0]}>
--- src/widgets/StyledWidget.jsx
+++ src/widgets/StyledWidget.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const WidgetStyle = styled.div`
   border: 2px solid blue;
-  border-radius: 25px;
 `;
 
 export default WidgetStyle;
